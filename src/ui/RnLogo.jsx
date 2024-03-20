83fe9353214c7b282a24eb61adeec7056c9a7eb0
Marco De Canal <marco.decanal@gmail.com>
2024-03-20 11:14:35 +0100

    AppBar superiore

---
+++ src/ui/RnLogo.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export default function RnLogo() {
+  return (
+    <img
+      alt="Logo Route Nazionale 2024"
+      src="Logo40.png"
+      width={40}
+      height={40}
+    />
+  );
+}
