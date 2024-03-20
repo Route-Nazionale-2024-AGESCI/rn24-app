1ad3c47838f330c7e63713da6dc93ad4b15d9746
Marco De Canal <marco.decanal@gmail.com>
2024-03-20 18:37:34 +0100

    montserrat: weight 100 - 900

--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ import reportWebVitals from "./reportWebVitals";
 // import "@fontsource/roboto/500.css";
 // import "@fontsource/roboto/700.css";
 
-import "@fontsource/montserrat";
+//import "@fontsource/montserrat";
+import "@fontsource/montserrat/100.css";
+import "@fontsource/montserrat/200.css";
+import "@fontsource/montserrat/300.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/800.css";
+import "@fontsource/montserrat/900.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
