723e7f7e8e61526c9ec66d417acd404109f536f3
Marco De Canal <marco.decanal@gmail.com>
2024-03-20 14:05:39 +0100

    lib: nomi mesi in italiano

---
+++ src/lib/italianDate.js
@@ -0,0 +1,14 @@
+export const italianMonth = [
+  "Gennaio",
+  "Febbraio",
+  "Marzo",
+  "Aprile",
+  "Maggio",
+  "Giugno",
+  "Luglio",
+  "Agosto",
+  "Settembre",
+  "Ottobre",
+  "Novembre",
+  "Dicembre",
+];
