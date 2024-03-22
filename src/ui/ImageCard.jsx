b63b133168311501eb7ac9cc0072348843b9828e
Marco De Canal <marco.decanal@gmail.com>
2024-03-22 14:48:16 +0100

    ImageCard: componente per Tracce di servizio

--- src/ui/ImageCard.jsx
+++ src/ui/ImageCard.jsx
@@ -14,23 +14,18 @@ export default function ImageCard({ imgSrc, imgAlt, title, subtitle }) {
       }}
     >
       <CardActionArea>
-        {/*
-            Put gradient from alpha 0 #404040 to alpha 100 #1e1e1e at 77%
-        */}
         <CardMedia
+          component="image"
           image={imgSrc}
           title={imgAlt}
-          style={{
-            background:
-              "linear-gradient(180deg, rgba(64,64,64,0) 0%, rgba(30,30,30,1) 77%)",
-          }}
           sx={{
             height: "120px",
           }}
         >
           <CardContent
             sx={{
-              border: "1px solid red",
+              background:
+                "linear-gradient(180deg, rgba(64,64,64,0) 0%, rgba(30,30,30,1) 77%)",
               pr: "16px",
               pb: "16px",
               pl: "16px",
