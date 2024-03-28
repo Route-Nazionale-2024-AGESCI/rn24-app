803bc0eb7763ae215fafbbffd541956d031ff021
Marco De Canal <marco.decanal@gmail.com>
2024-03-28 14:32:18 +0100

    AppBar logo with higher resolution

--- src/ui/RnLogo.jsx
+++ src/ui/RnLogo.jsx
@@ -4,7 +4,7 @@ export default function RnLogo() {
   return (
     <img
       alt="Logo Route Nazionale 2024"
-      src="Logo40.png"
+      src="Logo160.png"
       width={40}
       height={40}
     />
