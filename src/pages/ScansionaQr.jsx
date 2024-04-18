624818134992c18f29ab04022f4e81643e24d723
Marco De Canal <marco.decanal@gmail.com>
2024-04-18 18:19:28 +0200

    basic scan functionalities

--- src/pages/ScansionaQr.jsx
+++ src/pages/ScansionaQr.jsx
@@ -1,3 +1,95 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import { QrReader } from "react-qr-reader";
+
 export default function ScansionaQr() {
-  return <></>;
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  const addContactToAddressBook = (info) => {
+    console.log(info);
+  };
+
+  const handleScan = (data, error) => {
+    if (data) {
+      try {
+        setData(data.text);
+        //console.dir(data.text);
+        const contactInfo = JSON.parse(data.text);
+        if (
+          contactInfo.nome &&
+          contactInfo.cognome &&
+          contactInfo.telefono &&
+          contactInfo.email
+        ) {
+          addContactToAddressBook(contactInfo);
+        } else {
+          throw new Error(
+            "Il QR Code scansionato non contiene le informazioni di contatto corrette."
+          );
+        }
+      } catch (err) {
+        // Gestisci gli errori di parsing del JSON
+        setError(err.message);
+      }
+    }
+    if (error) {
+      setError(error.message);
+    }
+  };
+
+  const handleError = (err) => {
+    // Gestisci gli errori di lettura del QR Code
+    setError(err.message);
+  };
+
+  return (
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     height: "100vh",
+    //   }}
+    // >*}
+    <Box
+      sx={{
+        backgroundColor: "#ffffff",
+        padding: "8px",
+        borderRadius: "8px",
+        width: "300px",
+        height: "300px",
+      }}
+    >
+      {!data && !error && (
+        <QrReader
+          delay={300}
+          // onError={handleError}
+          onResult={handleScan}
+          //style={{ width: "300px", height: "300px" }}
+        />
+      )}
+      {data && (
+        <div>
+          <p>{data}</p>
+        </div>
+      )}
+      {error && (
+        <div>
+          <p>Errore: {error}</p>
+        </div>
+      )}
+      {(error || data) && (
+        <button
+          onClick={() => {
+            setError(null);
+            setData(null);
+          }}
+        >
+          Riprova
+        </button>
+      )}
+    </Box>
+    // </div>
+  );
 }
