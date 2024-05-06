1c32ea592356e57823de4a32c356b0382afbe374
Marco De Canal <marco.decanal@gmail.com>
2024-05-06 09:20:49 +0200

    fixing AppBar auth not requested
    AppBar should be rendered only on auth content, not in login page or code recovery page (until now, the only ones to be viewed by a user not logged in)

--- src/pages/layout/AppBarLayout.jsx
+++ src/pages/layout/AppBarLayout.jsx
@@ -1,8 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 
-import { AuthIsLoggedIn, AuthIsNotLoggedIn } from "../../contexts/auth";
+//import { AuthIsLoggedIn, AuthIsNotLoggedIn } from "../../contexts/auth";
 import AppBar from "../../ui/AppBar";
 
 export default function AppBarLayout() {
@@ -10,12 +11,15 @@ export default function AppBarLayout() {
     <Box sx={{ mt: "40px" }}>
       <AppBar />
       <Container sx={{ p: 0 }}>
+        {/* Questo non dovrebbe servire, perchè la navbar è presente solo nelle
+        pagine autenticate... ma vista la chiarezza dei requisiti, teniamola che non si sa mai!
         <AuthIsLoggedIn>   
           <Outlet />
         </AuthIsLoggedIn>
         <AuthIsNotLoggedIn>
           <Link to={'/login'}>Accedi</Link>
-        </AuthIsNotLoggedIn>
+        </AuthIsNotLoggedIn>*/}
+        <Outlet />
       </Container>
     </Box>
   );
