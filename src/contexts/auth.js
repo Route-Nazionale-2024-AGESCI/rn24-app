bb9747ef750aeac645fc8f02bd40c8510cd85d45
Marco De Canal <marco.decanal@gmail.com>
2024-05-08 09:12:24 +0200

    replacing window.navigation.href with react-router redirect

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,109 +1,109 @@
 import React, { useContext, useState, useEffect } from "react";
+import { redirect } from "react-router-dom";
 
 import axios from "../lib/api";
 import { getUser } from "../lib/dataManager/user";
 
 import * as authAPI from "../lib/api/auth";
 
-const LOCAL_STORAGE_TOKEN_KEY = 'atoken';
+const LOCAL_STORAGE_TOKEN_KEY = "atoken";
 
 const AuthStatus = Object.freeze({
-    LoggedIn:   Symbol("logged_in"),
-    LoggedOut:  Symbol("logged_out"),
-    Loading:  Symbol("loading"),
+  LoggedIn: Symbol("logged_in"),
+  LoggedOut: Symbol("logged_out"),
+  Loading: Symbol("loading"),
 });
 
 const defaultState = {
-    status: AuthStatus.LoggedOut,
-    token: null,
-    loginAction: async ({username, password}) => {},
-    logOut: () => {},
-    user: null,
-    isLoaded: false,
+  status: AuthStatus.LoggedOut,
+  token: null,
+  loginAction: async ({ username, password }) => {},
+  logOut: () => {},
+  user: null,
+  isLoaded: false,
 };
 
 export const AuthContext = React.createContext(defaultState);
 
 export const useAuth = () => {
-    return useContext(AuthContext);
-}
+  return useContext(AuthContext);
+};
 
 export const AuthIsLoggedIn = ({ children }) => {
-    const { isLoaded, status } = useContext(AuthContext);
-    return <>{isLoaded && status === AuthStatus.LoggedIn ? children : null}</>;
+  const { isLoaded, status } = useContext(AuthContext);
+  return <>{isLoaded && status === AuthStatus.LoggedIn ? children : null}</>;
 };
 
 export const AuthIsNotLoggedIn = ({ children }) => {
-    const { status, isLoaded } = useContext(AuthContext);
-    return <>{isLoaded && status === AuthStatus.LoggedOut ? children : null}</>;
+  const { status, isLoaded } = useContext(AuthContext);
+  return <>{isLoaded && status === AuthStatus.LoggedOut ? children : null}</>;
 };
 
 const AuthProvider = ({ children }) => {
-    const localToken = localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY);
-    const [status, setStatus] = useState(
-        localToken
-        ? AuthStatus.Loading
-        : AuthStatus.LoggedOut
-    );
-    const [token, setToken] = useState(localToken);
-    const [user, setUser] = useState(null);
+  const localToken = localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY);
+  const [status, setStatus] = useState(
+    localToken ? AuthStatus.Loading : AuthStatus.LoggedOut
+  );
+  const [token, setToken] = useState(localToken);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        if(token) {
-            const interceptor = axios.interceptors.request.use(
-                config => {
-                        config.headers['Authorization'] = `Token ${token}`;
-                        return config;
-                    },
-                    error => {
-                        return Promise.reject(error);
-                    }
-            );
-
-            localStorage.setItem(LOCAL_STORAGE_TOKEN_KEY, token);
-
-            getUser().then(res => {
-                setUser(res);
-                setStatus(AuthStatus.LoggedIn);
-            });
-            return () => {
-                axios.interceptors.request.eject(interceptor);
-            };
+  useEffect(() => {
+    if (token) {
+      const interceptor = axios.interceptors.request.use(
+        (config) => {
+          config.headers["Authorization"] = `Token ${token}`;
+          return config;
+        },
+        (error) => {
+          return Promise.reject(error);
         }
-        // removeAuthenticationHeader();
-    }, [token]);
-    
-    
-    const loginAction = async ({username, password}) => {
-      try {
-        const res = await authAPI.login({username, password});
+      );
 
-        if (res) {
-            setToken(res.token);
-            return;
-        }
-      } catch (error) {
-        throw error;
+      localStorage.setItem(LOCAL_STORAGE_TOKEN_KEY, token);
+
+      getUser().then((res) => {
+        setUser(res);
+        setStatus(AuthStatus.LoggedIn);
+      });
+      return () => {
+        axios.interceptors.request.eject(interceptor);
+      };
+    }
+    // removeAuthenticationHeader();
+  }, [token]);
+
+  const loginAction = async ({ username, password }) => {
+    try {
+      const res = await authAPI.login({ username, password });
+
+      if (res) {
+        setToken(res.token);
+        return;
       }
-    };
-  
-    const logOut = () => {
-      setToken(null);
-      localStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
-      setStatus(AuthStatus.LoggedOut);
-    //   removeAuthenticationHeader();
-      window.location.href = "/login";
-    };
-    
-    const isLoaded = AuthStatus.Loading !== status;
-    if(!isLoaded)
-        return 'Caricamento...';
+    } catch (error) {
+      throw error;
+    }
+  };
 
-    return (
-      <AuthContext.Provider value={{ status, token, loginAction, logOut, user, isLoaded }}>
-        {children}
-      </AuthContext.Provider>
-    );
+  const logOut = () => {
+    setToken(null);
+    localStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
+    setStatus(AuthStatus.LoggedOut);
+    //   removeAuthenticationHeader();
+    //window.location.href = "/login";
+    return redirect("/login");
+  };
+
+  const isLoaded = AuthStatus.Loading !== status;
+  if (!isLoaded) return "Caricamento...";
+
+  return (
+    <AuthContext.Provider
+      value={{ status, token, loginAction, logOut, user, isLoaded }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,32 +1,31 @@
 import axios from "axios";
-
+import { redirect } from "react-router-dom";
 
 const instance = axios.create({
-    baseURL: 'https://rn24-dev.fly.dev/api/v1/',
-    headers: {
-        post: {
-            'Content-Type': 'application/json'
-        }
-    }
+  baseURL: "https://rn24-dev.fly.dev/api/v1/",
+  headers: {
+    post: {
+      "Content-Type": "application/json",
+    },
+  },
 });
 
 instance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        const status = error.response?.status
-        if (
-            status === 401
-            || (
-                status === 403
-                && error.response.data.detail === "Non sono state immesse le credenziali di autenticazione."
-            )
-        ) {
-            // TODO: potremmo mettere un messaggio di sessione scaduta?
-            window.location.href = '/login';
-            return;
-        }
-        return Promise.reject(error);
-        
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+    if (
+      status === 401 ||
+      (status === 403 &&
+        error.response.data.detail ===
+          "Non sono state immesse le credenziali di autenticazione.")
+    ) {
+      // TODO: potremmo mettere un messaggio di sessione scaduta?
+      //window.location.href = '/login';
+      //return;
+      return redirect("/login");
+    }
+    return Promise.reject(error);
   }
 );
 
