23217c858ee965e9884f75046dd767327bac0adf
Marco De Canal <marco.decanal@gmail.com>
2024-06-23 18:16:09 +0200

    modal restyling

--- src/ui/LogoutModal.jsx
+++ src/ui/LogoutModal.jsx
@@ -1,4 +1,4 @@
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
@@ -6,32 +6,35 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Typography from "@mui/material/Typography";
 
+import AccessButton from "./AccessButton";
+
 import { useAuth } from "../contexts/auth";
 
 export default function LogoutModal({ open, onClose }) {
   const { logOut } = useAuth();
 
   return (
-    <Dialog open={open} onClose={onClose} aria-labelledby="logout-dialog-title">
-      <DialogTitle id="logout-dialog-title">
-        <Typography
-          fontSize={"20px"}
-          fontWeight={600}
-          color={"agesciPurple.main"}
-        >
+    <Dialog
+      open={open}
+      onClose={onClose}
+      aria-labelledby="logout-dialog-title"
+      slotProps={{
+        backdrop: { style: { backgroundColor: "rgba(109, 80, 149, 0.3)" } },
+      }}
+    >
+      <DialogTitle id="logout-dialog-title" textAlign={"center"}>
+        <Typography fontSize={"20px"} fontWeight={600}>
           Vuoi davvero disconnetterti dall'App?
         </Typography>
       </DialogTitle>
       <DialogContent>
-        <DialogContentText
-          sx={{ fontSize: "14px", fontWeight: 400, color: "#2B2B2B" }}
-        >
+        <DialogContentText sx={{ fontSize: "16px", color: "#000000" }}>
           Per poter usare nuovamente l'App dovrai riconnetterti: ti serviranno
           una connessione internet, il tuo codice socio e la tua password
         </DialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button autoFocus onClick={onClose}>
+        {/* <Button autoFocus onClick={onClose}>
           <Typography
             fontSize="16px"
             fontWeight={400}
@@ -39,8 +42,15 @@ export default function LogoutModal({ open, onClose }) {
           >
             Annulla
           </Typography>
-        </Button>
-        <Button
+        </Button> */}
+        <AccessButton
+          sx={{
+            marginY: "16px",
+            color: "agesciRed.main",
+            borderColor: "agesciRed.main",
+            width: "85%",
+            maxWidth: "400px",
+          }}
           onClick={() => {
             logOut();
           }}
@@ -49,7 +59,7 @@ export default function LogoutModal({ open, onClose }) {
           <Typography fontSize="16px" fontWeight={600} color="agesciRed.main">
             Esci
           </Typography>
-        </Button>
+        </AccessButton>
       </DialogActions>
     </Dialog>
   );
--- src/ui/UnsubscribeModal.jsx
+++ src/ui/UnsubscribeModal.jsx
@@ -1,4 +1,4 @@
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
@@ -6,32 +6,31 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Typography from "@mui/material/Typography";
 
+import AccessButton from "./AccessButton";
+
 export default function UnsubscribeModal({ open, onClose, unsubscribe }) {
   return (
     <Dialog
       open={open}
       onClose={onClose}
       aria-labelledby="unsubscribe-dialog-title"
+      slotProps={{
+        backdrop: { style: { backgroundColor: "rgba(109, 80, 149, 0.3)" } },
+      }}
     >
-      <DialogTitle id="unsubscribe-dialog-title">
-        <Typography
-          fontSize={"20px"}
-          fontWeight={600}
-          color={"agesciPurple.main"}
-        >
+      <DialogTitle id="unsubscribe-dialog-title" textAlign={"center"}>
+        <Typography fontSize={"20px"} fontWeight={600}>
           Vuoi davvero cancellare l'iscrizione all'evento?
         </Typography>
       </DialogTitle>
       <DialogContent>
-        <DialogContentText
-          sx={{ fontSize: "14px", fontWeight: 400, color: "#2B2B2B" }}
-        >
+        <DialogContentText sx={{ fontSize: "16px", color: "#000000" }}>
           Se l'evento ha un numero di posti limitato, potresti non poterti più
           iscrivere
         </DialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button autoFocus onClick={onClose}>
+        {/* <Button autoFocus onClick={onClose}>
           <Typography
             fontSize="16px"
             fontWeight={400}
@@ -39,12 +38,22 @@ export default function UnsubscribeModal({ open, onClose, unsubscribe }) {
           >
             Annulla
           </Typography>
-        </Button>
-        <Button onClick={unsubscribe} autoFocus>
+        </Button> */}
+        <AccessButton
+          onClick={unsubscribe}
+          autoFocus
+          sx={{
+            my: "16px",
+            color: "agesciRed.main",
+            borderColor: "agesciRed.main",
+            width: "85%",
+            maxWidth: "400px",
+          }}
+        >
           <Typography fontSize="16px" fontWeight={600} color="agesciRed.main">
-            Cancella
+            Conferma Eliminzione
           </Typography>
-        </Button>
+        </AccessButton>
       </DialogActions>
     </Dialog>
   );
