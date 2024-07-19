4cb40024681d13e6969f0846bf41ecf11c4bd50f
Marco De Canal <marco.decanal@gmail.com>
2024-07-19 08:50:50 +0200

    app icons, banner and manifest

--- src/ui/RnLogo.jsx
+++ src/ui/RnLogo.jsx
@@ -6,7 +6,7 @@ export default function RnLogo() {
     <Link to="/">
       <img
         alt="Logo Route Nazionale 2024"
-        src="/Logo160.png"
+        src="/256.png"
         width={40}
         height={40}
       />
