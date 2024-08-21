19915a4d85bdafaba521b9700affe7e3948709c5
Marco De Vito <marcodevito47@gmail.com>
2024-08-21 20:21:05 +0200

    Qr code minor enanchments (#12)
    * fix back navigation from qr code
    
    * qr code camera video zoom in preview square

--- src/pages/ScansionaQr.jsx
+++ src/pages/ScansionaQr.jsx
@@ -87,6 +87,12 @@ export default function ScansionaQr() {
             scanDelay={300}
             constraints={constraints}
             onResult={handleScan}
+            videoStyle={{
+              width: "180%",
+              height: "180%",
+              top: "-40%",
+              left: "-40%",
+            }}
             containerStyle={{
               borderRadius: "8px",
               border: "8px solid white",
