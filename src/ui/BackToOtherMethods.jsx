9afd0f8884f8e4a234b3a88fb64161b9abbec1d0
vito-marcode <marcodevito47@gmail.com>
2024-08-21 19:06:13 +0200

    fix back navigation from qr code

--- src/ui/BackToOtherMethods.jsx
+++ src/ui/BackToOtherMethods.jsx
@@ -1,6 +1,6 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
@@ -8,6 +8,7 @@ export default function BackToOtherMethods({
   back,
   backText = "Torna agli altri metodi",
 }) {
+  const navigate = useNavigate();
   return (
     <Box
       sx={{
@@ -19,7 +20,7 @@ export default function BackToOtherMethods({
         variant="text"
         startIcon={<ArrowBackIcon sx={{ color: "#ffffff" }} />}
         component={RouterLink}
-        to={back}
+        onClick={() => navigate(-1)}
       >
         <Typography
           fontSize="16px"
